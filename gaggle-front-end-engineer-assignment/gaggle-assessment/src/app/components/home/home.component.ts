216174d02d8fa45f2e15758f945b9208d843acff
Ben Bricker <bbricker09@gmail.com>
2023-01-21 11:21:39 -0500

    Table on home page

--- gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/components/home/home.component.ts
+++ gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/components/home/home.component.ts
@@ -1,10 +1,51 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface GaggleIncident {
+  id: number;
+  date: Date;
+  type: string;
+  status: string;
+}
+
+const INCIDENT_DATA: GaggleIncident[] = [
+  { id: 1, date: new Date('2023-01-20'), type: 'alert', status: 'open' },
+];
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  displayedColumns: string[] = ['select', 'date', 'type', 'status'];
+  incidents = new MatTableDataSource<GaggleIncident>(INCIDENT_DATA);
+  selection = new SelectionModel<GaggleIncident>(true, []);
 
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.incidents.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.incidents.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: GaggleIncident): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.id
+    }`;
+  }
 }
--- gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/shared/material.module.ts
+++ gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/shared/material.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatIconModule, MatMenuModule],
-  exports: [MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatCheckboxModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatCheckboxModule,
+  ],
 })
 export class MaterialModule {}
