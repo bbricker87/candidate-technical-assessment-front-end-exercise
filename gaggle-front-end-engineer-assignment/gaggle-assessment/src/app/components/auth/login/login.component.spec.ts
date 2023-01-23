39e83447f0fa99ba7d186779defd3504e30d1cbb
Ben Bricker <bbricker09@gmail.com>
2023-01-23 11:38:29 -0500

    A few more tests

--- gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/components/auth/login/login.component.spec.ts
+++ gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/components/auth/login/login.component.spec.ts
@@ -3,15 +3,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { FormFieldComponent } from '@app/components/form-field/form-field.component';
 import { MaterialModule } from '@app/shared/material.module';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationService } from '@app/services/authentication.service';
 
 const activatedRouteStub = {
   paramMap: {
     returnUrl: '',
   },
 };
+
+const authServiceStub = {
+  login: (username: string, password: string) => {
+    return { success: true };
+  },
+};
+
+const routerSpy = { navigate: jasmine.createSpy('navigate') };
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -21,7 +30,11 @@ describe('LoginComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent, FormFieldComponent],
       imports: [MaterialModule, RouterTestingModule, ReactiveFormsModule],
-      providers: [{ provide: ActivatedRoute, useValue: activatedRouteStub }],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteStub },
+        { provide: Router, useValue: routerSpy },
+        { provide: AuthenticationService, useValue: authServiceStub },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
@@ -31,5 +44,30 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a valid form', () => {
+    component.loginForm.controls['username'].setValue('test');
+    component.loginForm.controls['password'].setValue('validPassword1');
+
+    expect(component.loginForm.valid).toBeTruthy();
+  });
+
+  it('should have an invalid form', () => {
+    component.loginForm.controls['username'].setValue('test');
+    component.loginForm.controls['password'].setValue('invalidPassword');
+
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('should submit', () => {
+    spyOn(component, 'login');
+    component.loginForm.controls['username'].setValue('test');
+    component.loginForm.controls['password'].setValue('validPassword1');
+
+    const signOn = fixture.nativeElement.querySelector('.m-loginForm__button');
+    signOn.click();
+
+    expect(component.login).toHaveBeenCalledTimes(1);
   });
 });
--- gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/components/form-field/form-field.component.spec.ts
+++ gaggle-front-end-engineer-assignment/gaggle-assessment/src/app/components/form-field/form-field.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormFieldComponent } from './form-field.component';
 import { MaterialModule } from '@app/shared/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 describe('FormFieldComponent', () => {
   let component: FormFieldComponent;
@@ -21,5 +21,99 @@ describe('FormFieldComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display password visibility toggle', () => {
+    component.type = 'password';
+    fixture.detectChanges();
+
+    const toggle = fixture.nativeElement.querySelector(
+      '.a-formField__passwordVisibility'
+    );
+    expect(toggle).toBeTruthy();
+  });
+
+  it('should not display password visibility toggle', () => {
+    component.type = 'text';
+    fixture.detectChanges();
+
+    const toggle = fixture.nativeElement.querySelector(
+      '.a-formField__passwordVisibility'
+    );
+    expect(toggle).toBeFalsy();
+  });
+
+  it('should clear the input field', () => {
+    const control = new FormControl('defaultText');
+    component.control = control;
+    fixture.detectChanges();
+
+    const clearButton = fixture.nativeElement.querySelector(
+      '.a-formField__clear'
+    );
+    let textField = fixture.nativeElement.querySelector('.a-formField__input');
+
+    expect(clearButton).toBeTruthy();
+    expect(textField.value).toEqual('defaultText');
+
+    clearButton.click();
+    fixture.detectChanges();
+
+    expect(textField.value).toEqual('');
+  });
+
+  it('should display email error', () => {
+    const control = new FormControl('defaultText', [Validators.email]);
+    control.updateValueAndValidity();
+    control.markAsDirty();
+    component.control = control;
+    component.label = 'Test';
+    fixture.detectChanges();
+
+    const errors = fixture.nativeElement.querySelector('.a-formField__errors');
+
+    expect(errors.innerText).toEqual('Test must be a valid email');
+  });
+
+  it('should display minimum length error', () => {
+    const control = new FormControl('a', [Validators.minLength(3)]);
+    control.updateValueAndValidity();
+    control.markAsDirty();
+    component.control = control;
+    component.label = 'Test';
+    fixture.detectChanges();
+
+    const errors = fixture.nativeElement.querySelector('.a-formField__errors');
+
+    expect(errors.innerText).toEqual('Test must be at least 3 characters');
+  });
+
+  it('should display minimum length error', () => {
+    const control = new FormControl('12345678910', [Validators.maxLength(10)]);
+    control.updateValueAndValidity();
+    control.markAsDirty();
+    component.control = control;
+    component.label = 'Test';
+    fixture.detectChanges();
+
+    const errors = fixture.nativeElement.querySelector('.a-formField__errors');
+
+    expect(errors.innerText).toEqual(
+      'Test cannot have more than 10 characters'
+    );
+  });
+
+  it('should display pattern error', () => {
+    const control = new FormControl('1234', [Validators.pattern('[a-zA-Z]*')]);
+    control.updateValueAndValidity();
+    control.markAsDirty();
+    component.control = control;
+    component.label = 'Test';
+    component.patternError = 'cannot include digits';
+    fixture.detectChanges();
+
+    const errors = fixture.nativeElement.querySelector('.a-formField__errors');
+
+    expect(errors.innerText).toEqual('Test cannot include digits');
   });
 });
